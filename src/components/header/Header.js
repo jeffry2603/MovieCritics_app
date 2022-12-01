dc9755366fc36686326a419e822cac60fc51892f
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2022-12-01 23:03:23 +0530

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://www.metacritic.com/images/icons/metacritic-icon.svg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Movies</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+export default Header
